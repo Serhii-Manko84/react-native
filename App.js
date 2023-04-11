3a6f59f72e2c3bf60fe8843d86dd5d009c997d8a
Serhii Manko <msa01051984@gmail.com>
2023-04-11 17:22:56 +0300

    BTN

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, ImageBackground, StyleSheet, Text, TextInput, View, } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!! Hello World!! Hello World!!</Text>
+      <ImageBackground style={styles.image} source={require('./image/Bg.jpg')}>
+    <View style = {styles.form}>
+      <View>
+        <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
+      <TextInput style={styles.input} textAlign={'center'}/>
+      </View>
+      <View style={{marginTop: 20}}>
+        <Text style={styles.inputTitle}>PASSWORD</Text>
+      <TextInput style={styles.input} textAlign={'center'} secureTextEntry = {true}/>
+      </View>
+     <TouchableOpacity/>
+    </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +26,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  
+  image: {
+    flex:1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    // alignItems: 'center',
+  },
+  
+  form:{
+    marginHorizontal:40,
+  },
+
+  inputTitle:{
+    color: '#ffffff',
+    marginBottom: 10,
+    fontSize:18
+  }, 
+
+
+  input:{
+    borderWidth:1,
+    borderColor: '#ffffff',
+    height:40,
+    borderRadius: 6,
+    color: '#ffffff'
+  }
+
 });
