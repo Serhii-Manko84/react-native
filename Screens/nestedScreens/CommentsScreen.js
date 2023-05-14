7680ed95482844f6e1f8b1b4454117ea442eb161
Serhii Manko <msa01051984@gmail.com>
2023-05-14 17:04:10 +0300

    getAllComments

--- Screens/nestedScreens/CommentsScreen.js
+++ Screens/nestedScreens/CommentsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {
   View,
@@ -6,14 +6,21 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  SafeAreaView,
+  FlatList,
 } from "react-native";
 import { db } from "../../firebase/config";
-import { doc, addDoc, collection } from "firebase/firestore";
+import { doc, addDoc, collection, onSnapshot } from "firebase/firestore";
 
 const CommentsScreen = ({ route }) => {
   const postId = route.params.postId;
   const [comment, setComment] = useState("");
+  const [allComments, setAllComments] = useState([]);
   const { login } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    getAllComments();
+  }, []);
 
   const createComment = async () => {
     const docPostRef = await doc(db, "posts", postId);
@@ -21,10 +28,31 @@ const CommentsScreen = ({ route }) => {
       comment,
       login,
     });
+    setComment("");
+  };
+
+  const getAllComments = async () => {
+    const docPostRef = await doc(db, "posts", postId);
+    onSnapshot(collection(docPostRef, "comments"), (data) =>
+      setAllComments(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    );
   };
 
   return (
     <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={allComments}
+          renderItem={({ item }) => (
+            <View style={styles.containerComments}>
+              <Text>{item.login}</Text>
+              <Text>{item.comment}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+
       <TextInput
         style={styles.nameText}
         value={comment}
@@ -45,8 +73,17 @@ const CommentsScreen = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-end",
+    // justifyContent: "flex-end",
   },
+
+  containerComments: {
+    borderWidth: 1,
+    borderColor: "#FF6C00",
+    marginHorizontal: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+
   nameText: {
     fontFamily: "Roboto-BoldItalic",
     fontSize: 16,
